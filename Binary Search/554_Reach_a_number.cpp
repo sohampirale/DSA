187eee077e5ef61c1c0a17921fe3f5f537f8a5f3
sohampirale <162886548+sohampirale@users.noreply.github.com>
2024-10-07 10:38:52 +0000

    solved few questions on binary search

---
+++ Binary Search/554_Reach_a_number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+
+    int reachNumber(int target) {
+    	int sum=0,st=0;
+        target=abs(target);
+        while(sum<target||(sum-target)%2!=0){
+            st++;;
+            sum+=st;
+        }
+        return st;// i have looked up the solution so i wont submit it
+        //return st;
+    }
+};
---
+++ Binary Search/reach_a_number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int find(int target){
+        int st=0,end=1,cnt=0;
+        while(end!=target||end+1!=target){
+            int temp=end;
+            end=end+(end-st+1)
+            st=temp;
+            cnt++;
+        }
+        if(end==target)return cnt;
+        else return cnt+2;
+    }
+    int reachNumber(int target) {
+        return find(target);
+    }
+};
