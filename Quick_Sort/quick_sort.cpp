2b5cd2ed52e893758d125f78b9a38e1919f443b5
sohampirale <162886548+sohampirale@users.noreply.github.com>
2024-10-17 01:46:46 +0000

    quick sort bro code metyhod also learned (poivot at end and j,i ptrs moving from front till end)

--- Quick_Sort/quick_sort.cpp
+++ Quick_Sort/quick_sort.cpp
@@ -40,14 +40,11 @@ void quick_sort_pivot_at_mid(vector<int>&nums,int low,int high){
     int mid=st+(end-st)/2;
     int pivot=nums[mid];
     while(st<=end){
-
         while(nums[st]<pivot){
             st++;
         }
-        
         while(nums[end]>pivot)
             end--;
-
         if(st<=end){
             int temp=nums[st];
             nums[st]=nums[end];
@@ -55,7 +52,6 @@ void quick_sort_pivot_at_mid(vector<int>&nums,int low,int high){
             st++;
             end--;
         }
-     
         cout<<"Array modified to : ";
         for(int i=0;i<nums.size();i++)cout<<nums[i]<<" ";
         cout<<endl;
@@ -65,14 +61,37 @@ void quick_sort_pivot_at_mid(vector<int>&nums,int low,int high){
     quick_sort_pivot_at_mid(nums,st,high);
 }
 
+//j moving forward comparing with the pivot (th element at the nums[high])
+void quick_sort_brocode(vector<int>&nums,int low,int high){
+    if(low>=high)return;
+    int i=low-1,j=low;
+    cout<<"hi"<<endl;
+    int pivot=nums[high];
+    cout<<"hi"<<endl;
+    while(j!=high){
+       if(nums[j]<pivot){
+        int temp=nums[j];
+        i++;
+        nums[j]=nums[i];
+        nums[i]=temp;
+       }
+       j++;
+    }
+    i++;
+    nums[high]=nums[i];
+    nums[i]=pivot;
+    quick_sort_brocode(nums,low,i-1);
+    quick_sort_brocode(nums,i+1,high);
+}
 int main(){
+
     int n;
     cout<<"Enter size of array : ";
     cin>>n;
     vector<int>nums(n);
     cout<<"Enter "<<n<<" numbers : ";
     for(int i=0;i<n;i++)cin>>nums[i];
-    quick_sort_pivot_at_mid(nums,0,n-1);
+    quick_sort_brocode(nums,0,n-1);
     cout<<"After calling the quick sort fucntion the original array becomes : ";
     for(int i=0;i<n;i++)cout<<nums[i]<<" ";
 }
