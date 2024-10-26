3a007f5d3156ef9980672d1894cbedb2a370e9ca
sohampirale02 <sanjivanipirale@gmail.com>
2024-10-26 02:55:01 +0000

    bst practice

---
+++ Trees/bst.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node*left,*right;
+        node(int data){
+            this->data=data;
+            this->left=nullptr;
+            this->right=nullptr;
+        }
+};
+
+node* search(int data,node*t){
+    if(t==nullptr){
+        cout<<"Returning nullptr"<<endl;
+        return nullptr;
+    }
+    if(t->data==data)return t;
+    if(t->data<data)return search(data,t->right);
+    else return search(data,t->left);
+}
+
+string search(int data,node*t,string path){
+    if(t==nullptr){
+        path.clear();
+        return path;
+    }
+    if(t->data==data)return path ;
+    if(t->data<data) return search(data,t->right,path+"->"+to_string(t->data));
+    else return search(data,t->left,path+"->"+to_string(t->data));
+}
+
+
+void add(int data,node*&t){
+    if(t==nullptr){
+        cout<<"First node of BST  created"<<endl;
+        t=new node(data);
+        return;
+    }
+
+    if(data<t->data){
+        if(!t->left){
+            t->left=new node(data);
+            cout<<"CReated a left child of "<<t->data<<endl;
+            return;
+        } else {
+            add(data,t->left);
+        }
+    } else {
+        if(!t->right){
+            t->right=new node(data);
+            cout<<"Right child created of "<<t->data<<endl;
+            return;
+        } else {
+            add(data,t->right);
+        }
+    }
+}
+
+void free_nodes(node*t){
+    if(t==nullptr)return;
+    free_nodes(t->left);
+    free_nodes(t->right);
+    cout<<"Deleting "<<t->data<<endl;
+    delete t;
+}
+
+int main(){
+    int choice=1,data;
+    node*root=nullptr;
+    while(choice!=0){
+        cout<<"Enter your choice\n1 : Add \n2 : search\nYour choice : ";
+        cin>>choice;
+        if(choice==1){
+            cout<<"Enter the data : ";
+            cin>>data;
+            add(data,root);
+        } else if(choice==2){
+            cout<<"Enter the data to search : ";
+            cin>>data;
+
+            cout<<"1 : Get ptr\n2 : Get string\nYour chocie : ";
+            cin>>choice;
+            if(choice==1){
+                node*temp=search(data,root);
+                cout<<"Came out"<<endl;
+                if(temp)cout<<"Node found with data : "<<temp->data<<endl;
+                else cout<<"Node with data : "<<data<<" not found"<<endl;
+            } else if(choice==2){
+                string path=search(data,root,"root");
+                if(!path.empty()){
+                    cout<<"Node found wiht that data : ";
+                    cout<<"Path : "<<path<<endl;
+                } else cout<<"NOde with data : "<<data<<" not found"<<endl;
+            }            
+        }
+    }
+    free_nodes(root);
+}
