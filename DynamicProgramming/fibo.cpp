43eb7ae2fa2912b40746e99e49c56a79ae788698
Soham Pirale <23031089_cse@adcet.in>
2025-01-21 17:45:48 +0000

    Learned matrix Travelling right and down Top Left to Right Bottom & started learning can Sum question

--- DynamicProgramming/fibo.cpp
+++ DynamicProgramming/fibo.cpp
@@ -2,17 +2,21 @@
 #include<unordered_map>
 using namespace std;
 unordered_map<unsigned long long,unsigned long long>fiboMap={{0,0},{1,1},{2,1}};
-
+unsigned long long cnt=0;
 
 unsigned long long fibo(unsigned long long n){
     if(n<=2)return 1;
     if(fiboMap.find(n)==fiboMap.end()){
+        cnt++;
+        cout<<"Calculation no - "<<cnt<<endl;
         fiboMap[n]=fibo(n-1)+fibo(n-2);
     } 
     return fiboMap[n];
 }
 
 unsigned long long normalFibo(unsigned long long n){
+    cnt++;
+    cout<<"making calculation no "<<cnt<<endl;
     if(n==1||n==2)return 1;
     else return normalFibo(n-1)+normalFibo(n-2);
 }
--- DynamicProgramming/grid.cpp
+++ DynamicProgramming/grid.cpp
@@ -2,7 +2,10 @@
 #include<vector>
 #include<queue>
 #include<climits>
+#include<unordered_set>
+#include<unordered_map>
 using namespace std;
+
 class DynamicProgramming{
     public:
         int n=-1;
@@ -14,10 +17,13 @@ class DynamicProgramming{
         vector<vector<unsigned long long>>noOfWays;
         vector<vector<bool>>visited,pushed;
         int targetX,targetY;
+        unsigned long long cnt=0,savedCalculation=0;
         vector<pair<int,int>>downNright={{1,0},{0,1}};
         vector<pair<int,int>>leftNup={{-1,0},{0,-1}};
         vector<pair<int,int>>downNleftNright={{0,-1},{1,0},{0,1}};
+        vector<pair<int,int>>downNrightNdia={{1,0},{0,1},{1,1}};
         
+        unordered_set<unsigned long long>notPossible;
         bool isSafe(int x,int y){
             return (x>=0&&y>=0&&x<noOfWays.size()&&y<noOfWays[x].size());
         }
@@ -54,8 +60,11 @@ class DynamicProgramming{
 
         unsigned long long dynamicTraverse(int x,int y){
         if(noOfWays[x][y]!=0){
+            savedCalculation++;
             return noOfWays[x][y];
         }
+        // cnt++;
+        // cout<<"Calculation no - "<<cnt<<endl;
         for(auto it:downNright){
             int nextX=x+it.first;
             int nextY=y+it.second;
@@ -66,14 +75,17 @@ class DynamicProgramming{
         return noOfWays[x][y];
         }
 
-        unsigned long long normalTraverse(int x,int y){
+        unsigned long long matrixNormalTraverse(int x,int y){
+            cnt++;
+            cout<<"Calculation no - "<<cnt<<endl;
             if(x==targetX&&y==targetY)return 1;
             unsigned long long ret=0;
+
             for(auto it:downNright){
                 int nextX=x+it.first;
                 int nextY=y+it.second;
                 if(isSafe(nextX,nextY)){
-                    ret+=normalTraverse(nextX,nextY);
+                    ret+=matrixNormalTraverse(nextX,nextY);
                 }
             }
             return ret;
@@ -108,7 +120,7 @@ class DynamicProgramming{
             }
         }
 
-        void gridTopLeftToBottomRight(){
+        void gridTopLeftToBottomRightDynamic(){
             if(n==-1){
                 // cout<<"noOfWays matrix needs to be created"<<endl;
                 createMatrix();
@@ -116,6 +128,10 @@ class DynamicProgramming{
             noOfWays.assign(n,vector<unsigned long long>(n,0));
             noOfWays[targetX][targetY]=1;
             dynamicTraverse(0,0);
+            // cout<<"Saved total "<<savedCalculation<<" calculation from outside"<<endl;
+            savedCalculation=0;
+            cnt=0;
+            savedCalculation=0;
             cout<<"Dynamically traversed from Top-Left to Bottom-Right"<<endl;
             cout<<"Total no of ways to reach destination are : "<<noOfWays[0][0]<<endl;
         }
@@ -210,7 +226,7 @@ class DynamicProgramming{
             }
         }
 
-        int helper_matrixThreeDrxnTopLeftToBottomRight(int x,int y){
+        unsigned long long helper_matrixThreeDrxnTopLeftToBottomRight(int x,int y){
             if(noOfWays[x][y]!=0){
                 // cout<<"Returning "<<noOfWays[x][y]<<" from "<<x<<" & "<<y<<endl;
                 return noOfWays[x][y];
@@ -243,6 +259,144 @@ class DynamicProgramming{
             cout<<" visited cells are : "<<endl;
             displayVisitedMatrix();
         }
+
+        unsigned long long helper_matrixTopLeftToBottomRightAlvin(int x,int y){
+            if(x==0 || y==0)return 0;
+            else if(noOfWays[x][y]!=0)return noOfWays[x][y];
+
+            for(auto it: downNright){
+                int nextX=x+it.first;
+                int nextY=y+it.second;
+                noOfWays[x][y]+=helper_matrixTopLeftToBottomRightAlvin(nextX,nextY);
+            }
+
+            return noOfWays[x][y];
+        }
+
+        void matrixTopLeftToBottomRightAlvin(){
+            // unsigned long long totalPossibleWays=helper_matrixTopLeftToBottomRightAlvin(0,);
+            // cou
+        }
+
+        unsigned long long helper_matrixTopLeftToBottomRight_RightDownDia(int x,int y){
+            if(noOfWays[x][y]!=0){
+                return noOfWays[x][y];
+            }
+            cnt++;
+            cout<<"Calculation no - "<<cnt<<endl;
+            for(auto it:downNrightNdia){
+                int nextX=x+it.first;
+                int nextY=y+it.second;
+                if(isSafe(nextX,nextY)){
+                    noOfWays[x][y]+=helper_matrixTopLeftToBottomRight_RightDownDia(nextX,nextY);
+                }
+            }
+            return noOfWays[x][y];
+        }
+
+        void matrixTopLeftToBottomRight_RightDownDia(){
+            noOfWays.assign(n,vector<unsigned long long>(n,0));
+            noOfWays[targetX][targetY]=1;
+            int totalPossibleWays=helper_matrixTopLeftToBottomRight_RightDownDia(0,0);
+            cout<<"Traversal perfomed from Top Left to Bottom Right while moving(Right,Down & Diagonally) in : "<<totalPossibleWays<<endl;
+        }
+
+        unsigned long long helper_matrixTopLeftToBottomRight_invertIndexMarking(int x,int y){
+            if(noOfWays[x][y]!=0){
+                return noOfWays[x][y];
+            }
+            // cnt++;
+            // cout<<"Calculation no - "<<cnt<<endl;
+            for(auto it:downNright){
+                int nextX=x+it.first;
+                int nextY=y+it.second;
+                if(isSafe(nextX,nextY)){
+                    noOfWays[x][y]+=helper_matrixTopLeftToBottomRight_invertIndexMarking(nextX,nextY);
+                }
+            }
+            if(isSafe(y,x)){
+                noOfWays[y][x]=noOfWays[x][y];
+            }
+            return noOfWays[x][y];
+        }
+
+        void matrixTopLeftToBottomRight_invertIndexMarking(){
+            cnt=0;
+            noOfWays.assign(n,vector<unsigned long long>(n,0));
+            noOfWays[targetX][targetY]=1;
+            unsigned long long totalPossibleWays=helper_matrixTopLeftToBottomRight_invertIndexMarking(0,0);
+            cout<<"TRaversing complete from Top Left to Bottom Right using Marking the [y][x] after finding noOfWays[x][y] (Alvin)"<<endl;
+            cout<<"No of possible path from Top Left to Bottom Right are : "<<totalPossibleWays<<endl;
+            cnt=0;
+        }
+
+        bool helper_canSumDP(unsigned long long curr,vector<unsigned long long>&nums,unsigned long long target){
+            cout<<"Curr = "<<curr<<endl;
+            if(curr>target||notPossible.find(curr)!=notPossible.end()){
+                notPossible.insert(curr);
+                return false;
+            }
+            else if(curr==target)return true;
+            for(unsigned long long &next:nums){
+                if(helper_canSumDP(curr+next,nums,target))return true;
+            }
+            notPossible.insert(curr);
+            return false;
+        }
+
+        bool canSumDP(){
+            notPossible.clear();
+            unsigned long long target;
+            int nNum;
+            cout<<"ENter target : ";
+            cin>>target;
+            cout<<"How many number are in the vector : ";
+            cin>>nNum;
+            vector<unsigned long long>nums(nNum,0);
+            cout<<"Enter "<<nNum<<" numbers of the vector"<<endl;
+            for(int i=0;i<nNum;i++){
+                cin>>nums[i];
+            }
+            bool ans=helper_canSumDP(0,nums,target);
+            if(ans){
+                cout<<"It is possibel to reach sum = "<<target<<" using the vector"<<endl;
+            } else {
+                cout<<"it is not possibel to reach target with given numbers"<<endl;
+            }
+            return ans;
+        }
+
+        bool helper_canSum(unsigned long long curr,vector<unsigned long long>&nums,unsigned long long target){
+            cout<<"hey"<<endl;
+            if(curr==target)return true;
+            else if(curr>target)return false;
+            bool ret=false;
+            for(int next:nums){
+                if(helper_canSum(curr+next,nums,target))return true;
+            }
+            return false;
+        }
+
+        bool canSum(){
+            unsigned long long target;
+            int nNum;
+            cout<<"ENter target : ";
+            cin>>target;
+            cout<<"How many number are in the vector : ";
+            cin>>nNum;
+            vector<unsigned long long>nums(nNum,0);
+            cout<<"Enter "<<nNum<<" numbers of the vector"<<endl;
+            for(int i=0;i<nNum;i++){
+                cin>>nums[i];
+            }
+            bool ans=helper_canSum(0,nums,target);
+            if(ans){
+                cout<<"It is possibel to reach sum = "<<target<<" using the vector"<<endl;
+            } else {
+                cout<<"it is not possibel to reach target with given numbers"<<endl;
+            }
+            return ans;
+        }
 };
 
 int getChoice(){
@@ -255,6 +409,10 @@ int getChoice(){
     cout<<"6 : Display noOfWays matrix"<<endl;
     cout<<"7 : No of ways finding From Bottom Right to TOp Left Coming back"<<endl;
     cout<<"8 : From Top Left to Right Bottom using DP (Three Drxn-down,left,right)"<<endl;
+    cout<<"9 : Matrix from Top Left to Right Bottom In Right,Down & Diagonal Way"<<endl;
+    cout<<"10 : From Top Left to Bottom Right while marking [y][x] when noOfWays[x][y] found"<<endl;
+    cout<<"11 : Can Sum Recursion"<<endl;
+    cout<<"12 : Can Sum using DP"<<endl;
     cout<<"Your choice : ";
     cin>>choice;
     return choice;
@@ -269,10 +427,10 @@ int main(){
     while(choice){
         choice=getChoice();
         if(choice==1){
-            DP.gridTopLeftToBottomRight();
+            DP.gridTopLeftToBottomRightDynamic();
         } 
         else if(choice==2){
-            unsigned long long totalPossiblePaths=DP.normalTraverse(0,0);
+            unsigned long long totalPossiblePaths=DP.matrixNormalTraverse(0,0);
             cout<<"DFS traversal done from Top-Left to Bottom-Right"<<endl;
             cout<<"Total no of possible paths are : "<<totalPossiblePaths<<endl;
         }
@@ -299,7 +457,20 @@ int main(){
         else if(choice==8){
             DP.matrixThreeDrxnTopLeftToBottomRight();
         }
+        else if(choice==9){
+            DP.matrixTopLeftToBottomRight_RightDownDia();
+        }
+        else if(choice==10){
+            DP.matrixTopLeftToBottomRight_invertIndexMarking();
+        }
+        else if(choice==11){
+            DP.canSum();
+        }
+        else if(choice==12){
+            DP.canSumDP();
+        }
     }while(choice);
 }
+
 // 2399309444331282944
 // 8998663545468580096
